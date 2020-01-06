6d94d1fa6729b6cbc2c2e6a145e8d447116b3ff6
Neutralas <simonasburk@gmail.com>
2020-01-06 16:25:04 +0200

    išsprendžiau lol
    viskas gerokai paprasčiau, nei atrodė iš pradžių... :))

--- 4d6-1d6/main.cpp
+++ 4d6-1d6/main.cpp
@@ -6,24 +6,32 @@ int main()
 {
     ///kauliukai nuo 1 iki 6 akučių, 4d6 - 1d6 (min)
 
-    int Suma=0, unikalu=0, Kombinacijos[360], Viskas[4096], x=0; ///360, nes 6P4; 4096 nes 4^6;
+    int Viskas[1296], x=0, maz; /// 1296 nes 6^4;
+    float Suma=0;
 
-    for(int a=0; a<6; a++)
+    ///ieskau galimu kombinaciju
+    for(int a=1; a<=6; a++)
     {
-        for(int b=0; b<6; b++)
+        for(int b=1; b<=6; b++)
         {
-            for(int c=0; c<6; c++)
+            for(int c=1; c<=6; c++)
             {
-                for(int d=0; d<6; d++)
+                for(int d=1; d<=6; d++)
                 {
-                    if(/*nesutampa akuciu skaicius*/) ///gal parasyti funkcija? nes reikes dar pora kartu.
-                    {
-                        Kombinacijos[x]=1000*a+100*b+10*c+d;
-                        x++;
-                    }
+                    maz=min(min(a, b), min(c, d));
+                    Viskas[x]=a+b+c+d-maz;
+                    x++;
                 }
             }
         }
     }
+
+    for(int x=0; x<1296; x++)
+    {
+        Suma+=Viskas[x];
+    }
+
+    cout << Suma/1296;
+
     return 0;
 }
